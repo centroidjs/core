3efea28034c589c31e264c267aea56d5be71b737
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2024-12-01 15:22:35 +0000

    validate service container

--- spec/Service.spec.ts
+++ spec/Service.spec.ts
@@ -1,15 +1,9 @@
-import { ServiceContainer, injectable } from '@centroidjs/common';
+import { ServiceContainer } from '@centroidjs/common';
 
-@injectable()
 class AnotherService {
-    public readonly enabled = true;
+    public counter = 0;
 }
 
-@injectable({
-    providers: [
-        AnotherService
-    ]
-})
 class Service1 {
     constructor(protected another: AnotherService) {
         //
@@ -17,24 +11,19 @@ class Service1 {
     getMessage() {
         return {
             message: 'Hello World',
-            enabled: this.another.enabled
+            counter: this.another.counter++
         };
     }
 }
 
-@injectable({
-    providers: [
-        AnotherService
-    ]
-})
 class Service2 extends Service1 {
     constructor(protected another: AnotherService) {
         super(another);
     }
     getMessage() {
         return {
-            message: 'Hello World',
-            enabled: this.another.enabled
+            message: 'Hello World!',
+            counter: this.another.counter++
         };
     }
 }
@@ -42,26 +31,31 @@ class Service2 extends Service1 {
 describe('ServiceContainer', () => {
     it('should create instance', () => {
         const container = new ServiceContainer();
-        container.use(Service1);
-        const service1 = container.get(Service1);
+        container.useService(Service1, {
+            useFactory: () => new Service1(new AnotherService())
+        });
+        const service1 = container.getService(Service1);
         expect(service1).toBeTruthy();
-        expect(container.has(Service1)).toBeTruthy();
+        expect(container.hasService(Service1)).toBeTruthy();
     });
 
     it('should use service', () => {
         const container = new ServiceContainer();
-        container.use(Service1, Service2);
-        const service = container.get(Service1);
+        container.useService(AnotherService);
+        container.useService(Service1, {
+            useFactory: (serviceContainer) => new Service2(serviceContainer.getService(AnotherService))
+        });
+        const service = container.getService(Service1);
         expect(service).toBeTruthy();
         expect(service.getMessage().message).toEqual('Hello World!');
     });
 
     it('should delete service', () => {
         const container = new ServiceContainer();
-        container.use(Service1, Service2);
-        const service = container.get(Service1);
+        container.useService(Service1);
+        const service = container.getService(Service1);
         expect(service).toBeTruthy();
-        container.delete(Service1);
-        expect(container.has(Service1)).toBeFalsy();
+        container.removeService(Service1);
+        expect(container.hasService(Service1)).toBeFalsy();
     });
 });
--- src/ServiceContainer.ts
+++ src/ServiceContainer.ts
@@ -1,54 +1,84 @@
-declare type ApplicationServiceConstructor<T> = new (...args: unknown[]) => T;
-declare type InjectableApplicationService = new (...args: unknown[]) => unknown;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function injectable(configuration?: { providers: ApplicationServiceConstructor<unknown>[] }) {
-    return (target: InjectableApplicationService) => {
-        const original = target;
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const ctor: any = function (...args: unknown[]): unknown {
-            return new original(...args) as unknown;
-        }
-        const {prototype} = original;
-        Object.assign(ctor, {
-            prototype
-        });
-        return ctor;
-    };
-}
+declare type ServiceConstructor<T> = new (...args: unknown[]) => T;
 
+declare type InjectableServiceConstructor<T> = new (container: ServiceContainer, ...args: unknown[]) => T;
+
+declare type ServiceFactory<T> = (container: ServiceContainer, ...args: unknown[]) => T;
+
+/**
+ * A container for managing application services.
+ * 
+ * @class ServiceContainer
+ * @template T - The type of the service.
+ */
 class ServiceContainer {
 
-    private readonly services = new Map<string, unknown>;
+    protected readonly services = new Map<string, unknown>;
     
-    public get<T>(serviceConstructor?: ApplicationServiceConstructor<T>): T {
+    /**
+     * Retrieves a service instance from the service container.
+     *
+     * @template T - The type of the service to retrieve.
+     * @param {ServiceConstructor<T>} [serviceConstructor] - The constructor of the service to retrieve.
+     * @returns {T} The instance of the requested service.
+     */
+    public getService<T>(serviceConstructor?: ServiceConstructor<T>): T {
         return this.services.get(serviceConstructor.name) as T;
     }
 
-    public has<T>(serviceConstructor: ApplicationServiceConstructor<T>): boolean {
+    /**
+     * Checks if a service is registered in the service container.
+     *
+     * @template T - The type of the service.
+     * @param {ServiceConstructor<T>} serviceConstructor - The constructor of the service to check.
+     * @returns {boolean} - Returns `true` if the service is registered, otherwise `false`.
+     */
+    public hasService<T>(serviceConstructor: ServiceConstructor<T>): boolean {
         return this.services.has(serviceConstructor.name);
     }
 
-    public delete<T>(serviceConstructor: ApplicationServiceConstructor<T>): boolean {
+    /**
+     * Removes a service from the service container.
+     *
+     * @template T - The type of the service to be removed.
+     * @param {ServiceConstructor<T>} serviceConstructor - The constructor of the service to be removed.
+     * @returns {boolean} - Returns `true` if the service was successfully removed, otherwise `false`.
+     */
+    public removeService<T>(serviceConstructor: ServiceConstructor<T>): boolean {
         return this.services.delete(serviceConstructor.name);
     }
 
-    public use<T>(
-        serviceConstructor: ApplicationServiceConstructor<T>,
-        strategyConstructor?: ApplicationServiceConstructor<T>
+    /**
+     * Registers a service with the service container.
+     *
+     * @template T - The type of the service.
+     * @param {ServiceConstructor<T>} serviceConstructor - The constructor of the service to be registered.
+     * @param {ServiceConstructor<T> | { useFactory: ServiceFactory<T> }} [strategyConstructor] - Optional. Either a constructor for a strategy or an object containing a factory function to create the service.
+     * @returns {this} The current instance of the service container.
+     */
+    public useService<T>(
+        serviceConstructor: ServiceConstructor<T>,
+        strategyConstructor?: ServiceConstructor<T> | { useFactory: ServiceFactory<T> }
     ): this {
-        if (strategyConstructor) {
-            const strategy = new strategyConstructor();
+        if (typeof strategyConstructor === 'function') {
+            const strategy = new strategyConstructor(this);
             this.services.set(serviceConstructor.name, strategy);
             return this;
         }
-        this.services.set(serviceConstructor.name, new serviceConstructor());
+        if (typeof strategyConstructor === 'object' && typeof strategyConstructor.useFactory === 'function') {
+            const strategy = strategyConstructor.useFactory(this);
+            this.services.set(serviceConstructor.name, strategy);
+            return this;
+        }
+        this.services.set(serviceConstructor.name, new serviceConstructor(this));
         return this;
     }
 
 }
 
 export {
-    ServiceContainer,
-    injectable
+    ServiceConstructor,
+    InjectableServiceConstructor,
+    ServiceFactory,
+    ServiceContainer
 }
